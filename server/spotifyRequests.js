9f73f1847d3da5403c32cea799a470a549394e67
Hack Reactor Student <student@hackreactor.com>
2017-05-02 23:49:02 -0700

    created spotify helper file, with some information

---
+++ server/spotifyRequests.js
@@ -0,0 +1,14 @@
+import request from 'request';
+
+const Client_ID = '17af2e7d17c94de6b602b7ae29b1a7c0';
+const Client_Secret = 'e87dca1ab2094afd84f33f19fa73bef6';
+const baseURL = 'https://api.spotify.com/v1/';
+
+
+var findSong = function() {
+  
+}
+
+var findArtist = function() {
+
+}
