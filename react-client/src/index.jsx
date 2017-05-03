693a24497533cae0caee5f73f9382f51b1f2ee20
Anthony Cassara <acassara@gmail.com>
2017-05-03 11:19:07 -0700

    Modified git ignore with specific routes, added comment to index to test

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
+//this is the index
+
 ReactDOM.render(<App />, document.getElementById('app'));
