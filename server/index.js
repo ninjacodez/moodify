c9d7d7e523997bc57d081796612b2d839a917220
Yzhang1994 <yzhang1994@gmail.com>
2017-05-02 20:49:35 -0700

    trying to resolve merge issue

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const app = require('./app.js');
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4568;
 
 app.listen(PORT, () => {
   console.log('server listening on port: ', PORT);
