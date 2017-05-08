cf5817ed1c4b82f09cfabf80d3473412e77f56de
Yzhang1994 <yzhang1994@gmail.com>
2017-05-08 09:04:21 -0700

    clean up

--- database/index.js
+++ database/index.js
@@ -3,6 +3,7 @@ mongoose.Promise = require('bluebird');
 // mongoose.connect('mongodb://localhost/test');
 mongoose.connect('mongodb://heroku_6xrjjbsq:8g9mlf83h7sg407v23ikm97rso@ds133281.mlab.com:33281/heroku_6xrjjbsq');
 
+
 const db = mongoose.connection;
 
 db.on('error', () => {
