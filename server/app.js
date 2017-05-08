af8f274e12824b18be6dd354a2b4ec0b36a5ffb0
Mel hsieh <mehsieh89@gmail.com>
2017-05-08 16:41:07 -0700

    integrated spotify api call

--- server/app.js
+++ server/app.js
@@ -39,61 +39,99 @@ app.post('/fetchLyricsByTrackId', (req, res) => {
 
 app.post('/process', (req, res) => {
   let input = req.body;
+  let spotifyURI = '';
   return mmHelpers.getLyricsByTrackId(input.track_id)
   .then(data => {
     input.lyrics = data.lyrics.lyrics_body;
     var songEntry = new db.Song(input);
-
-    songEntry.save((err, songEntry) => {
-      // if (err) { throw error; }
-      console.log('saved: ', input.track_name);
-      res.send(input.lyrics);
-    });
+    songEntry.save()
+    //watson call 1?
+    return watsonHelpers.queryWatsonToneHelper(input.lyrics)
   })
-})
-
-app.post('/watson-tone', (req, res) => {
-  console.log('WATSON TONE POST');
-  return watsonHelpers.queryWatsonToneHelper(req.body.song)
-  .then(results => {
-    const watsonData = {
-      song: results.song,
-      anger: results.anger,
-      disgust: results.disgust,
-      fear: results.fear,
-      joy: results.joy,
-      sadness: results.sadness,
-      analytical: results.analytical,
-      confident: results.confident,
-      tentative: results.tentative,
-      openness: results.openness,
-      conscientiousness: results.conscientiousness,
-      extraversion: results.extraversion,
-      agreeableness: results.agreeableness,
-      emotionalrange: results.emotionalrange
+  .then(moods1 => {
+     let watsonData = {  
+      track_id: input.track_id,
+      anger: moods1.anger,
+      disgust: moods1.disgust,
+      fear: moods1.fear,
+      joy: moods1.joy,
+      sadness: moods1.sadness,
+      analytical: moods1.analytical,
+      confident: moods1.confident,
+      tentative: moods1.tentative,
+      openness: moods1.openness,
+      conscientiousness: moods1.conscientiousness,
+      extraversion: moods1.extraversion,
+      agreeableness: moods1.agreeableness,
+      emotionalrange: moods1.emotionalrange
     };
     const newEntry = new db.Watson(watsonData);
-    return newEntry.save()
-  })
-  .then(entry => {
-    console.log('ENTRY SAVED', entry);
-    res.send(JSON.stringify(entry));
-  })
-  .error(err => {
-    res.send(err);
-  });
-});
+    newEntry.save() //need return?
 
-app.post('/watson-nlu', (req, res) => {
-  console.log('WATSON NLU POST', req.body.song);
-  return watsonHelpers.queryWatsonNLUHelper(req.body.song)
-  .then(results => {
-    console.log('********************', results)
-    res.send(JSON.stringify(results));
-  })
-  .catch(err => {
-    res.send(err);
-  })
-});
+    return getSongByTitleAndArtist(input.track_name, input.artist_name)
+  })  
+  .then((spotifyData) => {
+    spotifyURI = spotifyData.uri
+    res.json([input.lyrics, watsonData, spotifyURI]);
+  })  
+
+  // .then(data => {
+  //   //watson call 2?
+  //   watsonHelpers.queryWatsonNLUHelper(input.lyrics)
+  // })
+  // .then(() => {
+  //   //write everything to db
+  //   var songEntry = new db.Song(input);
+  //   return songEntry.save()
+  // })
+  .catch(() => {
+    console.log('hello from catch')
+    res.send('error');
+  });
+})
+
+// app.post('/watson-tone', (req, res) => {
+//   console.log('WATSON TONE POST');
+//   return watsonHelpers.queryWatsonToneHelper(req.body.song)
+//   .then(results => {
+//     const watsonData = {
+//       song: results.song,
+//       anger: results.anger,
+//       disgust: results.disgust,
+//       fear: results.fear,
+//       joy: results.joy,
+//       sadness: results.sadness,
+//       analytical: results.analytical,
+//       confident: results.confident,
+//       tentative: results.tentative,
+//       openness: results.openness,
+//       conscientiousness: results.conscientiousness,
+//       extraversion: results.extraversion,
+//       agreeableness: results.agreeableness,
+//       emotionalrange: results.emotionalrange
+//     };
+//     const newEntry = new db.Watson(watsonData);
+//     return newEntry.save()
+//   })
+//   .then(entry => {
+//     console.log('ENTRY SAVED', entry);
+//     res.send(JSON.stringify(entry));
+//   })
+//   .error(err => {
+//     res.send(err);
+//   });
+// });
+
+// app.post('/watson-nlu', (req, res) => {
+//   console.log('WATSON NLU POST', req.body.song);
+//   return watsonHelpers.queryWatsonNLUHelper(req.body.song)
+//   .then(results => {
+//     console.log('********************', results)
+//     res.send(JSON.stringify(results));
+//   })
+//   .catch(err => {
+//     res.send(err);
+//   })
+// });
 
 module.exports = app;
