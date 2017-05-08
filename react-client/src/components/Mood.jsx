1e364eb345da76820f5dde6f2eb266c2c42849f3
Ryan Chow <chowryan@gmail.com>
2017-05-08 12:14:13 -0700

    Implementing additional charts

--- react-client/src/components/Mood.jsx
+++ react-client/src/components/Mood.jsx
@@ -1,41 +1,93 @@
 import React from 'react';
-import {Polar} from 'react-chartjs-2';
-
+import {Polar, Doughnut, Bar} from 'react-chartjs-2';
+import data from '../../../sampleWatsonData.js'
 class Mood extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: {
+      emotionData: {
         labels: ["Joy", "Fear", "Disgust", "Anger", "Sadness"],
         datasets: [{
             label: 'Kanye West - Famous',
-            data: [0.8, 1.3, 1.5, 5.6, 6.1],
+            data: [data.joy, data.fear, data.disgust, data.anger, data.sadness],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)'
             ],
             borderColor: [
                 'rgba(255,99,132,1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(153, 102, 255, 1)'
             ],
             borderWidth: 3
         }]
       },
-      options: {
+      emotionOptions: {
         title: {
           display: true,
           text: 'Kanye West - Famous',
           fontSize: 24
         }
-      }
+      },
+      languageData: {
+        labels: ["Analytical", "Confident", "Tentative"],
+        datasets: [{
+            label: 'Kanye West - Famous',
+            data: [data.analytical, data.confident, data.tentative],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 3
+        }]
+      },
+      languageOptions: {
+        title: {
+          display: true,
+          text: 'Kanye West - Famous',
+          fontSize: 24
+        }
+      },
+      socialData: {
+        labels: ["Openness", "Conscientiousness", "Extraversion", "Agreeableness", "Emotional Range"],
+        datasets: [{
+            label: 'Kanye West - Famous',
+            data: [data.openness, data.conscientiousness, data.extraversion, data.agreeableness, data.emotionalrange],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)'
+            ],
+            borderWidth: 3
+        }]
+      },
+      socialOptions: {
+        title: {
+          display: true,
+          text: 'Kanye West - Famous',
+          fontSize: 24
+        }
+      },
     };
   }
 
@@ -43,7 +95,9 @@ class Mood extends React.Component {
     return (
       <div className="item">
       <h4>Mood Component</h4>
-      <Polar data={this.state.data} options={this.state.options}/>
+      <Polar data={this.state.emotionData} options={this.state.emotionOptions} width={500}/>
+      <Doughnut data={this.state.languageData} options={this.state.languageOptions} width={500}/>
+      <Bar data={this.state.socialData} options={this.state.socialData} width={500}/>
       </div>
     )
   }
---
+++ sampleWatsonData.js
@@ -0,0 +1,19 @@
+const sampleWatsonData = {
+  "__v": 0,
+  "anger": 0.56756,
+  "disgust": 0.177621,
+  "fear": 0.138673,
+  "joy": 0.083,
+  "sadness": 0.575105,
+  "analytical": 0,
+  "confident": 0,
+  "tentative": 0.610268,
+  "openness": 0.064751,
+  "conscientiousness": 0.619853,
+  "extraversion": 0.793331,
+  "agreeableness": 0.940756,
+  "emotionalrange": 0.634517,
+  "_id": "5910ac6be28f258cfc35d15e"
+}
+
+export default sampleWatsonData;
