7d7177d04581925b7dd3249ab497173793ea9cff
Mel hsieh <mehsieh89@gmail.com>
2017-05-09 10:38:36 -0700

    fixed typo in app.jsx

--- react-client/src/components/App.jsx
+++ react-client/src/components/App.jsx
@@ -58,11 +58,6 @@ class App extends React.Component {
         // currentLyrics: '',
         // watson: {},
         // spotifyURI: ''
-        searchResultsLoading: false
-        currentSongNameAndArtist: ['Mel', "Melvo's Jams"],
-        currentLyrics: '',
-        watson: {},
-        spotifyURI: '',
       });
     });
   }
