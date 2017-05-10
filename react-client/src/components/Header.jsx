d1d5d8a6c2ff18ea85801bf0d44fab5d4ec38b7e
Anthony Cassara <acassara@gmail.com>
2017-05-10 16:58:06 -0700

    CSS implemented

--- react-client/src/components/Header.jsx
+++ react-client/src/components/Header.jsx
@@ -25,7 +25,6 @@ class Header extends React.Component {
       <div id="header" onClick={this.redirect}>
       <h3>moodify</h3>
       <img id="logo" src="./img/moodify.png" width="80" height="80"/>
-      <img id="mascot" src="./img/cow.png" width="10" height="16"/>
       </div>)
   }
 }
