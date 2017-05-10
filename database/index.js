1b77c8ca81f7b2285b51105ea5f773544e8d4573
Mel hsieh <mehsieh89@gmail.com>
2017-05-09 18:32:40 -0700

    finished basic authentication

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-// mongoose.connect('mongodb://localhost/test');
-mongoose.connect('mongodb://heroku_6xrjjbsq:8g9mlf83h7sg407v23ikm97rso@ds133281.mlab.com:33281/heroku_6xrjjbsq');
+mongoose.connect('mongodb://localhost/test');
+// mongoose.connect('mongodb://heroku_6xrjjbsq:8g9mlf83h7sg407v23ikm97rso@ds133281.mlab.com:33281/heroku_6xrjjbsq');
 
 const db = mongoose.connection;
 
