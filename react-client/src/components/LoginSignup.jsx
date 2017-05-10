36efd970730f790388f17ae83b8b2d64909704b3
Yzhang1994 <yzhang1994@gmail.com>
2017-05-10 14:57:35 -0700

    delete jquery in login/signup

--- react-client/src/components/LoginSignup.jsx
+++ react-client/src/components/LoginSignup.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Header.jsx';
-import $ from "jquery";
 import { Redirect, Link } from 'react-router-dom';
 import axios from 'axios';
 
