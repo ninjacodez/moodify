eb5b84ac94acaf38ff87a6e260c3dc6508a77c4c
yzhang1994 <yzhang1994@gmail.com>
2017-05-10 17:37:54 -0700

    add past search component and listeners in app.js (#94)
    * add react router
    
    * add user table in database/index.js, add methods in auth.js
    
    * add react router
    
    * add user songs parameter in user schema, improve musixmatch search functionality, add hash.js
    
    * add react router
    
    * add hashing function to protect user passwords
    
    * delete jquery
    
    * implement axios
    
    * delete jquery
    
    * implement axios
    
    * add config into .gitignore
    
    * add axios to package.josn
    
    * delete jquery in router.jsx
    
    * delete jquery in login/signup
    
    * delete jquery
    
    * implement axios
    
    * delete jquery
    
    * add config into .gitignore
    
    * add axios to package.josn
    
    * delete jquery in router.jsx
    
    * delete jquery in login/signup
    
    * add react router
    
    * add hashing function to protect user passwords
    
    * delete jquery
    
    * add config into .gitignore
    
    * fix error in App.jsx

---
+++ react-client/src/components/PastSearches.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import axios from 'axios';
+
+class PastSearches extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      songArray: []
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    axios.get('/pastSearches')
+    .then(res => {
+      this.setState({ songArray: res.data });
+    })
+    .catch(err => { console.log(err)})
+  }
+
+  render() {
+    return (
+      <div>
+        <button onclick={this.handleClick}>Past Searches</button>
+        {this.state.songArray.map((song, i) => {
+          return (
+            <div key={i}> {song.track_name} by {song.artist_name} </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default PastSearches;
