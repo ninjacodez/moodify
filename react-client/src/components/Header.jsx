5834a9de141fc23a3b8776afe3d8e3eb678614d9
Anthony Cassara <acassara@gmail.com>
2017-05-10 22:31:17 -0700

    Cow logo is back!

--- react-client/src/components/Header.jsx
+++ react-client/src/components/Header.jsx
@@ -24,10 +24,11 @@ class Header extends React.Component {
     return (
       <div id="header" onClick={this.redirect}>
       <h3>moodify</h3>
-      <img id="logo" src="./img/moodify.png" width="80" height="80"/>
-      <img id="mascot" src="./img/cow.png" width="10" height="16"/>
+      <img id="mascot" src="./img/cow.png" width="75" height="75"/>
       </div>)
   }
 }
 
 export default Header;
+
+      // <img id="logo" src="./img/moodify.png" width="80" height="80"/>
