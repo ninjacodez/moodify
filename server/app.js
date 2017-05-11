3554c371bd590539651c539cb78c23f55512763c
Ryan Chow <chowryan@gmail.com>
2017-05-10 17:02:46 -0700

    Fixed database saving async issues (#91)

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
 // dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+// const path = require('path');
 const cors = require('cors');
-const Promise = require('bluebird');
+// const Promise = require('bluebird');
 
 // other module exports
 const auth = require('./auth.js');
@@ -36,7 +36,7 @@ app.post('/search', (req, res) => {
 });
 
 app.post('/fetchLyricsByTrackId', (req, res) => {
-  let trackId = req.body.trackId;
+  const trackId = req.body.trackId;
   return mmHelpers.getLyricsByTrackId(trackId)
   .then(lyrics => {
     res.send(lyrics);
@@ -46,44 +46,49 @@ app.post('/fetchLyricsByTrackId', (req, res) => {
 
 app.post('/process', (req, res) => {
   let input = req.body;
-  let songNameAndArtist = [input.artist_name, input.track_name];
+  const songNameAndArtist = [input.artist_name, input.track_name];
   let watsonData = {};
-  let spotifyURI = '';
+
   return mmHelpers.getLyricsByTrackId(input.track_id)
   .then(data => {
     const lyrics = data.lyrics.lyrics_body;
 
-    // filters copyright
     input.lyrics = lyrics.slice(0, (lyrics.indexOf('*******')));
 
-    var songEntry = new db.Song(input);
-    songEntry.save()
-    //watson call 1?
+    const songEntry = new db.Song(input);
+    return songEntry.save(err => {
+      if (err) console.log ("SAVE SONG ERROR", err);
+    })
+  })
+  .then(() => {
     return watsonHelpers.queryWatsonToneHelper(input.lyrics)
   })
-  .then(moods1 => {
+  .then(data => {
     watsonData = {
       track_id: input.track_id,
-      anger: moods1.anger,
-      disgust: moods1.disgust,
-      fear: moods1.fear,
-      joy: moods1.joy,
-      sadness: moods1.sadness,
-      analytical: moods1.analytical,
-      confident: moods1.confident,
-      tentative: moods1.tentative,
-      openness: moods1.openness,
-      conscientiousness: moods1.conscientiousness,
-      extraversion: moods1.extraversion,
-      agreeableness: moods1.agreeableness,
-      emotionalrange: moods1.emotionalrange
+      anger: data.anger,
+      disgust: data.disgust,
+      fear: data.fear,
+      joy: data.joy,
+      sadness: data.sadness,
+      analytical: data.analytical,
+      confident: data.confident,
+      tentative: data.tentative,
+      openness: data.openness,
+      conscientiousness: data.conscientiousness,
+      extraversion: data.extraversion,
+      agreeableness: data.agreeableness,
+      emotionalrange: data.emotionalrange
     };
     const newEntry = new db.Watson(watsonData);
-    newEntry.save() //need return?
-
+    return newEntry.save(err => {
+      if (err) console.log('SAVE WATSON ERROR: ', err);
+    })
+  })
+  .then(() => {
     return spotifyHelpers.getSongByTitleAndArtist(input.track_name, input.artist_name)
   })
-  .then((spotifyData) => {
+  .then(spotifyData => {
     res.json([songNameAndArtist, input.lyrics, watsonData, spotifyData]);
   })
   // .then(data => {
@@ -96,8 +101,8 @@ app.post('/process', (req, res) => {
   //   return songEntry.save()
   // })
   .catch((error) => {
-    console.log(error)
-    res.send('hi');
+    console.log('/PROCESS ERROR: ', error);
+    res.send(error);
   });
 })
 
