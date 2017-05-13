131081f6339dbba4285d7535a95297b19ea3d0f4
Mel hsieh <mehsieh89@gmail.com>
2017-05-12 17:25:42 -0700

    fixed user adding songs

--- database/index.js
+++ database/index.js
@@ -24,7 +24,8 @@ let songSchema = mongoose.Schema({
 	album_coverart_350x350: String,
 	album_coverart_500x500: String,
 	album_coverart_800x800: String,
-	lyrics: String
+	lyrics: String,
+  spotify_uri: String
 });
 songSchema.plugin(beautifyUnique);
 const Song = mongoose.model('Song', songSchema);
