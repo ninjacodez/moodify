184a2fa00e100f7123a494829dc661f596ecfaa7
Aly-Tamboura <alytamboura7370@gmail.com>
2017-05-17 12:21:48 -0700

    remove console logs

--- react-client/src/components/TopRow.jsx
+++ react-client/src/components/TopRow.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const TopRow = (props) => {
-	console.log(props)
 	return (
 		<li  className='searchText' onClick={() => {props.newReleaseClick(props.rows.artists[0].uri)}}><span>{props.rows.name}</span>&nbsp;-{props.rows.artists[0].name}</li>
 	)
