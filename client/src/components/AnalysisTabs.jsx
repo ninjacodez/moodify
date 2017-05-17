5a2563ab17c469fa3e2ad7783a73da39e46b72c6
Ramsha Mushtaq <ramsha.a.mushtaq@gmail.com>
2017-05-17 10:14:45 -0700

    Create UI tabs and add dummy data to Music Analysis Tab

--- client/src/components/AnalysisTabs.jsx
+++ client/src/components/AnalysisTabs.jsx
@@ -8,28 +8,24 @@ import Mood from './Mood.jsx';
 class AnalysisTabs extends React.Component {
   constructor(props) {
     super(props);
-
-    // this.state = {
-    //   tabsArray: [],
-    // }
   }
 
   render() {
     return (
       <Tabs>
         <TabList>
-          <Tab >Lyrics Analysis</Tab>
           <Tab >Music Analysis</Tab>
+          <Tab >Lyrics Analysis</Tab>
         </TabList>
-
-        <TabPanel>
-          <h3>Lyrics Analysis</h3>
-          <Mood watson={this.props.watson} songNameAndArtist={this.props.songNameAndArtist} ></Mood>
-        </TabPanel>
 
         <TabPanel>
           <h3>Mood Analysis</h3>
           <MusicAnalysis watson={this.props.watson} songNameAndArtist={this.props.songNameAndArtist} ></MusicAnalysis>
+        </TabPanel>
+
+        <TabPanel>
+          <h3>Lyrics Analysis</h3>
+          <Mood watson={this.props.watson} songNameAndArtist={this.props.songNameAndArtist} ></Mood>
         </TabPanel>
       </Tabs>
     );
