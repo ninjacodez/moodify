d4af08884b8164052e83262d71e6712d464f022c
Ramsha Mushtaq <ramsha.a.mushtaq@gmail.com>
2017-05-16 18:57:19 -0700

    modify AnalysisTabs component

--- react-client/src/components/AnalysisTabs.jsx
+++ react-client/src/components/AnalysisTabs.jsx
@@ -4,6 +4,9 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 // import mood component (lyrics analysis)
 // import songMood component (song analysis)
 
+import SongAnalysis from './SongAnalysis';
+import Mood from './Mood';
+
 class AnalysisTabs extends React.Component {
   constructor(props) {
     super(props);
@@ -23,12 +26,12 @@ class AnalysisTabs extends React.Component {
 
         <TabPanel>
           <h3>Lyrics Analysis</h3>
-          <Mood ></Mood>
+          <Mood watson={this.props.watson} songNameAndArtist={this.props.currentSongNameAndArtist} ></Mood>
         </TabPanel>
 
         <TabPanel>
           <h3>Mood Analysis</h3>
-          <SongAnalysis ></SongAnalysis>
+          <MusicAnalysis ></MusicAnalysis>
         </TabPanel>
       </Tabs>
     );
