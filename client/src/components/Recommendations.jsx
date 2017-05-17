b019a169e6a639b7efed66da0abf1af42c7faf92
Ramsha Mushtaq <ramsha.a.mushtaq@gmail.com>
2017-05-17 12:10:59 -0700

    Create Recommendations Component

---
+++ client/src/components/Recommendations.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Recommendations extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // state? yes maybe - only when clicked on should it appear???
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Recommendations</h1>
+      </div>
+    );
+  }
+};
+
+export default Recommendations;
