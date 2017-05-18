6a853acff073532476b8664901580a67e6ab323a
James Hogan <jhogan@jhogan.net>
2017-05-17 21:20:55 -0700

    Implement Spotify API authentication

--- server/spotifyHelpers.js
+++ server/spotifyHelpers.js
@@ -1,29 +1,78 @@
 const Promise = require('bluebird');
-const request = Promise.promisifyAll(require('request'));
+const config = require('./config');
 
-const getSongByTitleAndArtist = (title, artist) => {
-  const rootURL = 'https://api.spotify.com/v1/';
-  console.log('before the api request ...')
-  return request.getAsync(rootURL + 'search?q=track:' + title + '%20artist:' + artist + '&type=track')
-  .then(data => {
-    console.log('api response received...')
-    let parsedData = JSON.parse(data.body);
-    return parsedData.tracks.items[0].uri;
+const axios = require('axios');
+const querystring = require('querystring');
+const authenticationParsers = require('www-authenticate').parsers;
+
+const SPOTIFY_AUTHENTICATION_URL = 'https://accounts.spotify.com/api/token';
+const SPOTIFY_ROOT_URL = 'https://api.spotify.com/v1';
+const SPOTIFY_SEARCH_URL = `${SPOTIFY_ROOT_URL}/search`;
+
+const requestAccessToken = () => {
+  const qs = querystring.stringify({ 'grant_type': 'client_credentials' });
+  const axiosConfig = {
+    auth: {
+      username: config.SPOTIFY_API_KEY.id,
+      password: config.SPOTIFY_API_KEY.secret,
+    }
+  };
+
+  return axios.post( SPOTIFY_AUTHENTICATION_URL, qs, axiosConfig )
+  .then( response => {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.access_token}`;
   })
-  .catch(err => { console.log(err); });
+  .catch( err => {
+    console.log( 'failed token request' );
+    console.log( err );
+  });
 };
 
-// const Client_ID = '17af2e7d17c94de6b602b7ae29b1a7c0';
-// const Client_Secret = 'e87dca1ab2094afd84f33f19fa73bef6';
+const isExpiredTokenError = function( err ) {
+  let authHeader = err.response.headers['www-authenticate'];
+  let parsedHeader = new authenticationParsers.WWW_Authenticate( authHeader );
+  if ( parsedHeader &&
+       parsedHeader.parms &&
+       (parsedHeader.parms.error_description === 'The access token expired') ) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-//use/refactor with query-parser??
-//can adjust API call with field filters, currently all functions search via name
-  //ex. track named, artist named, album named, etc.
-//add other functions based on what to search on API
-//use URI retrieved from API request / other things (?)
-  //can embed on front-end via iframes (look into)
-//results are in arrays of objects
-  //should consider what we want out of them
-  //also how to manipulate data
+const authenticatedRequest = function( requestFunction, ...args ) {
+  return requestFunction( ...args )
+  .catch( err => {
 
+    if ( isExpiredTokenError(err) ) {
+      return requestAccessToken()
+      .then( () => {
+        return requestFunction( ...args );
+      });
+
+    } else {
+      return Promise.reject(err);
+    }
+  });
+};
+
+const getSongByTitleAndArtistRequest = function( title, artist ) {
+  const axiosConfig = {
+    params: {
+      q: `track:${title} artist:${artist}`,
+      type: 'track',
+    },
+  };
+
+  return axios.get( SPOTIFY_SEARCH_URL, axiosConfig )
+  .then( response => {
+    return response.data.tracks.items[0].uri;
+  });
+};
+
+const getSongByTitleAndArtist = function( title, artist ) {
+  return authenticatedRequest( getSongByTitleAndArtistRequest, title, artist );
+};
+
+module.exports.requestAccessToken = requestAccessToken;
 module.exports.getSongByTitleAndArtist = getSongByTitleAndArtist;
