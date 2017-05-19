1adf13580d75f9ab88d0f0ecf55631bab1d1c9eb
jiaconis <john.t.iaconis@gmail.com>
2017-05-18 17:44:03 -0700

    (feat) Search functionality has been split into books and songs

--- react-client/src/components/Search.jsx
+++ react-client/src/components/Search.jsx
@@ -19,7 +19,7 @@ class Search extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    this.props.search(this.state.title, this.state.artist);
+    this.props.search(this.state.title, this.state.artist, e.target.id);
     this.setState({ title: '', artist: '', showPrev: true});
   }
 
@@ -32,22 +32,33 @@ class Search extends React.Component {
   render() {
     return (
       <div>
-      <div className="search">
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" name="title" value={this.state.title} placeholder="title" onChange={this.handleTitleChange} className="textbox"/>
-          <input type="text" name="artist" value={this.state.artist} placeholder="artist" onChange={this.handleArtistChange} className="textbox"/>
-          <input type="submit" value="Search" className="submitbutton"/>
+        <div className="search">
+          <form onSubmit={this.handleSubmit}>
+            <input type="text"
+                   name="song title"
+                   value={this.state.title}
+                   placeholder="title"
+                   onChange={this.handleTitleChange}
+                   className="textbox"/>
+            <input type="text" name="Artist"
+                   value={this.state.artist}
+                   placeholder="artist"
+                   onChange={this.handleArtistChange}
+                   className="textbox"/>
+            <button id="/search" onClick={this.handleSubmit} className="submitbutton">Search Songs</button>
+            <button id="/books" onClick={this.handleSubmit} className="submitbutton">Search Books</button>
           </form>
+
           <form>
-          {this.props.showPrev ?
-            <div className="resultsBar" onClick={this.prevResults} >
-            <div className="searchHeadline">Search Results</div>
-            {this.props.upDown ?
-            <img className="searchPrevUp" src="./img/ic_down.svg" width="18" height="18"/>
-            : <img className="searchPrevDown" src="./img/ic_up.svg" width="18" height="18"/>}
-            </div>
-            : null}
-        </form>
+            {this.props.showPrev ?
+              <div className="resultsBar" onClick={this.prevResults} >
+              <div className="searchHeadline">Search Results</div>
+              {this.props.upDown ?
+              <img className="searchPrevUp" src="./img/ic_down.svg" width="18" height="18"/>
+              : <img className="searchPrevDown" src="./img/ic_up.svg" width="18" height="18"/>}
+              </div>
+              : null}
+          </form>
         </div>
       </div>
     );
@@ -55,3 +66,22 @@ class Search extends React.Component {
 }
 
 export default Search;
+
+
+/*
+          <form onSubmit={this.bookSubmit}>
+          <input type="text" 
+                 name="title" 
+                 value={this.state.bookTitle} 
+                 placeholder="book title" 
+                 onChange={this.handleBookTitleChange} 
+                 className="textbox"/>
+          <input type="text" 
+                 name="Author" 
+                 value={this.state.author} 
+                 placeholder="author" 
+                 onChange={this.handleAuthorChange} 
+                 className="textbox"/>
+          <input type="submit" value="Search" className="submitbutton"/>
+          </form>
+*/
